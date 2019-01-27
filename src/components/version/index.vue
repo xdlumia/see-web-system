aec6858a5d8ac8da49e79eabf17df4388c211ec5
xd Wang <xdwang@xddeiMac.lan>
2019-01-27 09:29:43 +0800

    修改版本更新

--- src/components/version/index.vue
+++ src/components/version/index.vue
@@ -9,17 +9,30 @@
 <template>
   <div class="d-content main-content">
     <div class="bb pb10 f12">版本更新记录</div>
-    <ul class="version-content" v-loading="loading">
-      <div v-show="!versionList.length">暂无更新内容</div>
-      <view-pic>
-        <li v-for="(item,index) of versionList" :key="index">
-          <div class="content">
-            <h3>{{item.title}} <time>{{item.updatedDate | timeToStr('YYYY-MM-DD H:mm:ss')}}</time></h3>
-            <div class="details" v-html="item.details || '-'"></div>
-          </div>
-        </li>
-      </view-pic>
-    </ul>
+    <el-scrollbar style="height:calc(100% - 60px)">
+      <ul class="version-content" v-loading="loading">
+        <div v-show="!versionList.length">暂无更新内容</div>
+        <view-pic>
+          <li v-for="(item,index) of versionList" :key="index">
+            <div class="content">
+              <h3>{{item.title}} <time>{{item.updatedDate | timeToStr('YYYY-MM-DD H:mm:ss')}}</time></h3>
+              <div class="details" v-html="item.details || '-'"></div>
+            </div>
+          </li>
+        </view-pic>
+      </ul>
+    </el-scrollbar>
+    <el-pagination
+      class="mt5"
+      size="small"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="queryForm.page"
+      :page-sizes="[15, 20, 30, 40]"
+      :page-size="queryForm.limit"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="count">
+    </el-pagination>
   </div>
 </template>
 <script>
@@ -32,33 +45,46 @@ export default {
       loading:false,
       // 版本列表数据
       versionList:[],
+      count:0, //总数
       // 查询表单
       queryForm: {
-        limit:999,
+        limit:20,
         page:1,
         platformCode: this.$local.fetch('userInfo').syscode,
       },
     }
   },
   created () {
-    this.getVersionList()
+    this.getVersionList(this.queryForm)
   },
   methods: {
       // 获取表格数据
-      getVersionList() {
+      getVersionList(params) {
         this.loading = true
-        axios.get(baseURL.systemService + '/versionupdating/list',{params: this.queryForm})
+        axios.get(baseURL.systemService + '/versionupdating/list',{params: params})
         .then(res => {
+          this.queryForm.page = res.curr
+          this.count = res.count
           this.versionList = res.data || []
         })
         .finally(()=>{
           this.loading = false
         })
       },
+      // 跳转
+      handleSizeChange(page){
+        this.queryForm.page = page
+        this.getVersionList(this.queryForm)
+      },
+      // 分页
+      handleCurrentChange(page){
+        this.queryForm.page = page
+        this.getVersionList(this.queryForm)
+      },
     }
 }
 </script>
-<style scoped lang="scss">
+<style lang="scss">
 .version-content{
     margin-top:10px; color: #333;
     .content{ flex:1}
