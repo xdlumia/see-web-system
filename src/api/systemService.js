05776390ca0d409c2bfc2ce3999fe6bf7761e9ca
xdlumia <xdq@live.cn>
2019-03-20 16:42:20 +0800

    修改服务

---
+++ src/api/systemService.js
@@ -0,0 +1,54 @@
+/**
+ * @author 王晓冬
+ * @description systemService的url
+ * @date 2019/1/11
+ * @host 9011
+ */
+
+import {Api} from 'see-web-basic'
+let baseURL = window.g.ApiUrl
+
+
+export default {
+   /**
+   * @description 业务平台
+   */
+    //  登录
+    login (params) {
+        return Api.post(baseURL.systemService + '/login/businessLogin', params,{isShowMessage:false})
+    },
+    //  登录
+    getUserInfo (params) {
+        return Api.get(baseURL.systemService + '/rmUser/userInfo', params)
+    },
+    updatePassword(params) {
+        return Api.post(baseURL.systemService + '/rmUser/updateUserPwdAfterLogin', params)
+    },
+    //  登出
+    logout (params) {
+        return Api.get(baseURL.systemService + '/login/logout', params,{isShowMessage:false})
+    },
+
+    /**
+    * @description 业务平台
+    */
+
+    //  版本更新提醒
+    versionRemind (params) {
+        return Api.get(baseURL.systemService + '/versionupdating/remind', params,{isShowMessage:false})
+    },
+
+    /**
+    * @description 考勤管理
+    */
+
+    // 获取考勤列表
+    getLoginfoList (params) {
+        return Api.get(baseURL.systemService + '/rmuserloginfo/list', params,)
+    },
+    // 获取考勤统计
+    getLoginStats (params) {
+        return Api.get(baseURL.systemService + '/rmuserloginfo/count', params,)
+    },
+
+}
