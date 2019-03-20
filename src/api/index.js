c130097ca0e71ccbe9d978b1873acc0cac93c955
xdlumia <xdq@live.cn>
2019-03-20 16:40:56 +0800

    添加服务

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,13 @@
 import { apiConfig } from 'see-web-basic';
 
 import bizSystemService from './bizSystemService';
+import systemService from './systemService';
 import resourceService from './resourceService';
 import seeDictionaryService from './seeDictionaryService';
 
-apiConfig.registry({ bizSystemService: bizSystemService,resourceService: resourceService, seeDictionaryService: seeDictionaryService }, false);
+apiConfig.registry({ 
+    bizSystemService: bizSystemService,
+    systemService:systemService,
+    resourceService: resourceService, 
+    seeDictionaryService: seeDictionaryService 
+}, false);
