4870981e0ab355128970f01f0f2715c913560f2a
zhaolun <zhaolun_git@fanteren.com>
2019-11-05 17:12:40 +0800

    see-web-system@1.4.1-beta3

--- src/components/businessSetting/companyAccount/add.vue
+++ src/components/businessSetting/companyAccount/add.vue
@@ -7,7 +7,7 @@
  -->
 <template>
   <div class v-loading="loading">
-    <el-form :model="companyForm" size="small" ref="companyForm" :rules="companyFormRules">
+    <el-form :model="companyForm" :rules="companyFormRules" ref="companyForm" size="small">
       <form-card title="公司财务信息">
         <el-row :gutter="40">
           <el-col :span="8">
@@ -46,7 +46,7 @@
         <el-row :gutter="40">
           <el-col :span="8">
             <el-form-item label="发票最大限额" prop="invoiceSum">
-              <d-select class="wfull" v-model="companyForm.invoiceSum" dicCode="PSI_GSSZ_FPZDXE"></d-select>
+              <d-select class="wfull" dicCode="PSI_GSSZ_FPZDXE" v-model="companyForm.invoiceSum"></d-select>
             </el-form-item>
           </el-col>
           <el-col :span="8">
@@ -64,27 +64,29 @@
       <form-card :title="true" v-if="!companyForm.commonCorporationAccountEntities.length">
         <div slot="title" style="display:flex;justify-content:space-between">
           <span>公司账户</span>
-          <el-button type="text" style="padding:0" @click="addAccount">+新建</el-button>
+          <el-button @click="addAccount" style="padding:0" type="text">+新建</el-button>
         </div>
       </form-card>
-      <form-card
-        :title="index < 1"
-        v-for="(item, index) in companyForm.commonCorporationAccountEntities"
-        :key="index"
-        class="mb10"
-      >
-        <div v-if="index < 1" slot="title" style="display:flex;justify-content:space-between">
+      <form-card :key="index" :title="index < 1" class="mb10" v-for="(item, index) in companyForm.commonCorporationAccountEntities">
+        <div slot="title" style="display:flex;justify-content:space-between" v-if="index < 1">
           <span>公司账户</span>
-          <el-button type="text" style="padding:0" @click="addAccount">+新建</el-button>
+          <el-button @click="addAccount" style="padding:0" type="text">+新建</el-button>
         </div>
         <el-row :gutter="40" style="position: relative">
           <el-col :span="8">
             <el-form-item
-              label="账户类型"
               :prop="'commonCorporationAccountEntities.' + index + '.accountType'"
               :rules="commonCorporationAccountEntitiesRules.accountType"
+              label="账户类型"
             >
-              <d-select class="wfull" v-model="item.accountType" dicCode="PSI_GSSZ_ZHLX" :disabled="index==0"></d-select>
+              <el-select :disabled="index==0" class="wfull" dicCode="PSI_GSSZ_ZHLX" v-model="item.accountType">
+                <el-option
+                  :key="index"
+                  :label="item.content"
+                  :value="item.code"
+                  v-for="(item,index) of (index==0?dictionaryOptions('PSI_GSSZ_ZHLX'):dictionaryOptions('PSI_GSSZ_ZHLX').filter(a=>a.code!='PSI_GSSZ_ZHLX-3'))"
+                ></el-option>
+              </el-select>
             </el-form-item>
           </el-col>
           <el-col :span="8" v-if="accountType(index) !== 3">
@@ -98,36 +100,36 @@
           </el-col>
           <el-col :span="8" v-if="accountType(index) === 5">
             <el-form-item
-              label="开户银行（到支行）"
               :prop="'commonCorporationAccountEntities.' + index + '.accountBank'"
               :rules="commonCorporationAccountEntitiesRules.accountBank"
+              label="开户银行（到支行）"
             >
               <el-input v-model.trim="item.accountBank"></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="8" v-if="accountType(index) === 5">
             <el-form-item
-              label="银行预留手机号"
               :prop="'commonCorporationAccountEntities.' + index + '.phone'"
               :rules="commonCorporationAccountEntitiesRules.phone"
+              label="银行预留手机号"
             >
               <el-input v-model.trim="item.phone"></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="8" v-if="accountType(index) === 5">
             <el-form-item
-              label="开户账号"
               :prop="'commonCorporationAccountEntities.' + index + '.account'"
               :rules="commonCorporationAccountEntitiesRules.account"
+              label="开户账号"
             >
               <el-input v-model.trim="item.account"></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="8">
             <el-form-item
-              label="账户状态"
               :prop="'commonCorporationAccountEntities.' + index + '.state'"
               :rules="commonCorporationAccountEntitiesRules.state"
+              label="账户状态"
             >
               <el-select class="wfull" v-model.trim="item.state">
                 <el-option :value="1" label="启用"></el-option>
@@ -139,41 +141,41 @@
             <el-form-item
               :label="accountType(index) === 1 ? '支付宝收款二维码' : '微信收款二维码'"
               :prop="'commonCorporationAccountEntities.' + index + '.qrCode'"
-              :rules="commonCorporationAccountEntitiesRules.qrCode"
               :ref="'qrCode'+index"
+              :rules="commonCorporationAccountEntitiesRules.qrCode"
             >
               <div class="wfull" style="display:flex;">
                 <upload-file
-                  class="mr10"
-                  v-model.trim="item.qrCode"
                   :limit="{type: ['jpg', 'jpeg', 'png']}"
-                  v-if="!item.qrCode"
                   :size="0.5"
                   @uploadSuccess="removeValidate(index)"
+                  class="mr10"
+                  v-if="!item.qrCode"
+                  v-model.trim="item.qrCode"
                 >
                   <slot>
-                    <div style class="ba upload-wrapper">
+                    <div class="ba upload-wrapper" style>
                       <span class="el-icon-plus" style="font-size:36px;color:#999;"></span>
                     </div>
                   </slot>
                 </upload-file>
-                <div style class="ba upload-wrapper mr10" v-else>
-                  <img style="width:100%;height:100%;object-fit: cover;" :src="item.qrCode" />
+                <div class="ba upload-wrapper mr10" style v-else>
+                  <img :src="item.qrCode" style="width:100%;height:100%;object-fit: cover;" />
                 </div>
                 <p class="d-text-qgray f12" style="display: flex; flex-direction: column">
                   <span>仅支持jpg、jpeg、png格式</span>
                   <span>图片大小500KB以下,推荐分辨率600*600</span>
-                  <el-button style="width: 65px;" type="text" @click="item.qrCode = ''">清空图片</el-button>
+                  <el-button @click="item.qrCode = ''" style="width: 65px;" type="text">清空图片</el-button>
                 </p>
               </div>
             </el-form-item>
           </el-col>
           <el-button
-            style="position:absolute;padding:0;right:20px;top:-5px;border:none;"
             @click="companyForm.commonCorporationAccountEntities.splice(index, 1)"
+            style="position:absolute;padding:0;right:20px;top:-5px;border:none;"
             v-if="index>0"
           >
-            <i style="font-size:20px;" class="el-icon-remove"></i>
+            <i class="el-icon-remove" style="font-size:20px;"></i>
           </el-button>
         </el-row>
       </form-card>
@@ -197,25 +199,31 @@ export default {
         invoiceSum: '',
         jinvoiceTaxRate: '',
         minLimitNum: '',
-        commonCorporationAccountEntities: [{
-          accountType: 'PSI_GSSZ_ZHLX-3',
-          accountName: '',
-          accountBank: '',
-          phone: '',
-          account: '',
-          state: 1,
-          qrCode: ''
-        }]
+        commonCorporationAccountEntities: [
+          {
+            accountType: 'PSI_GSSZ_ZHLX-3',
+            accountName: '',
+            accountBank: '',
+            phone: '',
+            account: '',
+            state: 1,
+            qrCode: ''
+          }
+        ]
       },
       companyFormRules: {
         corporationName: { required: true, message: '请输入', trigger: 'blur' },
         accountBank: { required: true, message: '请输入', trigger: 'blur' },
-        account: [{ required: true, message: '请输入', trigger: 'blur' },
-          { type: 'bankCard', message: '请输入正确的银行账号', trigger: 'blur' }],
+        account: [
+          { required: true, message: '请输入', trigger: 'blur' },
+          { type: 'bankCard', message: '请输入正确的银行账号', trigger: 'blur' }
+        ],
         taxpayersNum: { required: true, message: '请输入', trigger: 'blur' },
         address: { required: true, message: '请输入', trigger: 'blur' },
-        phone: [{ required: true, message: '请输入', trigger: 'blur' },
-          { type: 'phone', message: '请输入正确的手机号', trigger: 'blur' }],
+        phone: [
+          { required: true, message: '请输入', trigger: 'blur' },
+          { type: 'phone', message: '请输入正确的手机号', trigger: 'blur' }
+        ],
         invoiceSum: { required: true, message: '请选择', trigger: 'change' },
         jinvoiceTaxRate: { required: true, message: '请输入', trigger: 'blur' },
         minLimitNum: { required: true, message: '请输入', trigger: 'blur' }
@@ -224,14 +232,18 @@ export default {
         accountType: { required: true, message: '请选择', trigger: 'change' },
         accountName: { required: true, message: '请输入', trigger: 'blur' },
         accountBank: { required: true, message: '请输入', trigger: 'blur' },
-        phone: [{ required: true, message: '请输入', trigger: 'blur' },
-          { type: 'phone', message: '请输入正确的手机号', trigger: 'blur' }],
-        account: [{ required: true, message: '请输入', trigger: 'blur' },
-          { type: 'bankCard', message: '请输入正确的银行账号', trigger: 'blur' }],
+        phone: [
+          { required: true, message: '请输入', trigger: 'blur' },
+          { type: 'phone', message: '请输入正确的手机号', trigger: 'blur' }
+        ],
+        account: [
+          { required: true, message: '请输入', trigger: 'blur' },
+          { type: 'bankCard', message: '请输入正确的银行账号', trigger: 'blur' }
+        ],
         state: { required: true, message: '请选择', trigger: 'change' },
         qrCode: { required: true, message: '请上传', trigger: 'change' }
       }
-    }
+    };
   },
   props: {
     isEdit: {
@@ -243,46 +255,54 @@ export default {
       default: null
     }
   },
-  components: {
-  },
+  components: {},
   mounted() {
     if (this.editId && this.isEdit) {
-      this.commoncorporationInfo(this.editId)
+      this.commoncorporationInfo(this.editId);
     }
   },
   methods: {
     // 移除图片上传校验
     removeValidate(index) {
-      this.$refs.companyForm.clearValidate('commonCorporationAccountEntities.' + index + '.qrCode')
+      this.$refs.companyForm.clearValidate(
+        'commonCorporationAccountEntities.' + index + '.qrCode'
+      );
     },
     // 获取详情
     commoncorporationInfo(id) {
-      this.loading = true
-      this.$api.seeBaseinfoService.commoncorporationInfo(null, id).then(res => {
-        Object.keys(this.companyForm).forEach(key => {
-          this.companyForm[key] = res.data[key] || this.companyForm[key]
-          this.companyForm.account = this.companyForm.commonCorporationAccountEntities[0].account;
-          this.companyForm.accountBank = this.companyForm.commonCorporationAccountEntities[0].accountBank;
+      this.loading = true;
+      this.$api.seeBaseinfoService
+        .commoncorporationInfo(null, id)
+        .then(res => {
+          Object.keys(this.companyForm).forEach(key => {
+            this.companyForm[key] = res.data[key] || this.companyForm[key];
+            this.companyForm.account = this.companyForm.commonCorporationAccountEntities[0].account;
+            this.companyForm.accountBank = this.companyForm.commonCorporationAccountEntities[0].accountBank;
+          });
         })
-      }).finally(() => {
-        this.loading = false
-      })
+        .finally(() => {
+          this.loading = false;
+        });
     },
     // 保存
     saveCompany() {
       this.$refs.companyForm.validate(valid => {
         if (valid) {
-          this.loading = true
-          const port = this.isEdit ? 'commoncorporationUpdate' : 'commoncorporationSave'
-          this.companyForm.commonCorporationAccountEntities[0].account = this.companyForm.account
-          this.companyForm.commonCorporationAccountEntities[0].accountBank = this.companyForm.accountBank
-          this.$api.seeBaseinfoService[port](this.companyForm).then(res => {
-            this.$emit('refresh')
-          }).finally(() => {
-            this.loading = false
-          })
+          this.loading = true;
+          const port = this.isEdit
+            ? 'commoncorporationUpdate'
+            : 'commoncorporationSave';
+          this.companyForm.commonCorporationAccountEntities[0].account = this.companyForm.account;
+          this.companyForm.commonCorporationAccountEntities[0].accountBank = this.companyForm.accountBank;
+          this.$api.seeBaseinfoService[port](this.companyForm)
+            .then(res => {
+              this.$emit('refresh');
+            })
+            .finally(() => {
+              this.loading = false;
+            });
         }
-      })
+      });
     },
     // 添加公司账户
     addAccount() {
@@ -294,33 +314,37 @@ export default {
         account: '',
         state: '',
         qrCode: ''
-      })
+      });
     },
     // 获取账户类型
     accountType(index) {
-      return parseFloat(this.companyForm.commonCorporationAccountEntities[index].accountType.replace('PSI_GSSZ_ZHLX-', ''))
+      return parseFloat(
+        this.companyForm.commonCorporationAccountEntities[
+          index
+        ].accountType.replace('PSI_GSSZ_ZHLX-', '')
+      );
     },
     // 账户名称label
     accountNameLabel(index) {
-      let name
+      let name;
       switch (this.accountType(index)) {
         case 3:
-          name = '开户'
+          name = '开户';
           break;
         case 1:
-          name = '支付宝账户'
+          name = '支付宝账户';
           break;
         case 2:
-          name = '微信账户'
+          name = '微信账户';
           break;
         default:
-          name = '账户'
+          name = '账户';
           break;
       }
-      return name + '名称'
+      return name + '名称';
     }
   }
-}
+};
 </script>
 
 <style scoped lang='scss'>
