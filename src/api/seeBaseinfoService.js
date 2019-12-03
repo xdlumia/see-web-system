bbf138c1d833f5e9b5a84b96a40caa5628546ea9
zhaolun <zhaolun_git@fanteren.com>
2019-12-03 18:14:20 +0800

    baseinfo接口

--- src/api/seeBaseinfoService.js
+++ src/api/seeBaseinfoService.js
@@ -2,7 +2,7 @@
  * @Author: web.王晓冬
  * @Date: 2019-11-05 17:30:22
  * @LastEditors: 赵伦
- * @LastEditTime: 2019-12-03 17:29:39
+ * @LastEditTime: 2019-12-03 17:49:50
  * @Description: file content
  */
 /**
@@ -247,6 +247,130 @@ export default {
   // 获取公司账号
   getCompanyAccountList: {
     url: "/commoncorporation/select"
-  }
+  },
   /* =============公司账户设置表end=========== */
+  /* =============表单管理start=========== */
+  // 获取表单列表
+  formList: {
+    'url': '/form/list/{catagoryCode}',
+    'mock': false
+  },
+
+  // 获取表单相关所有信息
+  formQueryCatagoryCodeForm: {
+    'url': '/form/queryCatagoryCodeForm',
+    'mock': false
+  },
+  /* =============表单管理end=========== */
+
+  /* =============字段列表管理start=========== */
+  // 删除字段
+  formfieldsDeleteFormFields: {
+    'url': '/formfields/deleteFormFields',
+    'mock': false,
+    'methods': 'delete'
+  },
+
+  // 获取表单全部字段列表
+  formfieldsList: {
+    'url': '/formfields/list/{formId}',
+    'mock': false
+  },
+
+  // 获取表单全部字段列表
+  formfieldsQueryFieldsByForm: {
+    'url': '/formfields/queryFieldsByForm/{formCode}',
+    'mock': false
+  },
+
+  // 根据表单id,获取类型分组字段列表
+  formfieldsQueryFieldsTypeList: {
+    'url': '/formfields/queryFieldsTypeList',
+    'mock': false
+  },
+
+  // 添加字段
+  formfieldsSaveFormFields: {
+    'url': '/formfields/saveFormFields',
+    'mock': false,
+    'methods': 'post'
+  },
+  /* =============字段列表管理end=========== */
+
+  /* =============模板管理start=========== */
+  // 复制
+  templateCopy: {
+    'url': '/template/copy/{id}',
+    'mock': false
+  },
+
+  // 删除
+  templateDelete: {
+    'url': '/template/delete',
+    'mock': false,
+    'methods': 'delete'
+  },
+
+  // 查看详细信息
+  templateInfo: {
+    'url': '/template/info/{id}',
+    'mock': false
+  },
+
+  // 模板列表
+  templateList: {
+    'url': '/template/list/{formId}',
+    'mock': false
+  },
+
+  // 保存
+  templateSave: {
+    'url': '/template/save',
+    'mock': false,
+    'methods': 'post'
+  },
+
+  // 修改
+  templateUpdate: {
+    'url': '/template/update',
+    'mock': false,
+    'methods': 'post'
+  },
+
+  // 查看自定义字段
+  templateUserfields: {
+    'url': '/template/userfields/{id}',
+    'mock': false
+  },
+  /* =============模板管理end=========== */
+
+  /* =============模板分类管理start=========== */
+  // 删除模板类别
+  templatecategoryDelete: {
+    'url': '/templatecategory/delete',
+    'mock': false,
+    'methods': 'delete'
+  },
+
+  // 获取模板分类列表
+  templatecategoryList: {
+    'url': '/templatecategory/list',
+    'mock': false
+  },
+
+  // 保存模板类别
+  templatecategorySave: {
+    'url': '/templatecategory/save',
+    'mock': false,
+    'methods': 'post'
+  },
+  /* =============模板分类管理end=========== */
+
+  /* =============服务健康检查start=========== */
+  // 检查服务健康状况
+  healthCheck: {
+    'url': '/ws/health/check',
+    'mock': false
+  },
+  /* =============服务健康检查end=========== */
 }
